1acce2cca187b34d124f88c6376014043ddf8cff
acidDrain <jmgrow@gmail.com>
2020-10-13 02:17:34 -0500

    Created basic type definitions

---
+++ src/attributes.d.ts
@@ -0,0 +1,14 @@
+declare namespace NpcGen {
+  export interface Attributes {
+    Melee: { priority: number, defaultPriority: number, value: number };
+    Ranged: { priority: number, defaultPriority: number, value: number };
+    Strength: { priority: number, defaultPriority: number, value: number };
+    Dexterity: { priority: number, defaultPriority: number, value: number };
+    Constitution: { priority: number, defaultPriority: number, value: number };
+    Intelligence: { priority: number, defaultPriority: number, value: number };
+    Wisdom: { priority: number, defaultPriority: number, value: number };
+    Charisma: { priority: number, defaultPriority: number, value: number };
+    Luck: { priority: number, defaultPriority: number, value: number };
+  }
+}
+
