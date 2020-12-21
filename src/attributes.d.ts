4408f121959c3b55abde1ae8434cc07bbc8932ef
acidDrain <jmgrow@gmail.com>
2020-12-21 16:14:16 -0600

    WIP

--- src/attributes.d.ts
+++ src/attributes.d.ts
@@ -1,13 +1,96 @@
 declare namespace NpcGen {
-  export interface Attributes {
-    Melee: { priority: number; defaultPriority: number; value: number };
-    Ranged: { priority: number; defaultPriority: number; value: number };
-    Strength: { priority: number; defaultPriority: number; value: number };
-    Dexterity: { priority: number; defaultPriority: number; value: number };
-    Constitution: { priority: number; defaultPriority: number; value: number };
-    Intelligence: { priority: number; defaultPriority: number; value: number };
-    Wisdom: { priority: number; defaultPriority: number; value: number };
-    Charisma: { priority: number; defaultPriority: number; value: number };
-    Luck: { priority: number; defaultPriority: number; value: number };
+  enum AttributeNames {
+    Charisma = "CHARISMA",
+    Dexterity = "DEXTERITY",
+    Melee = "MELEE",
+    Intelligence = "INTELLIGENCE",
+    Wisdom = "WISDOM",
+    Constitution = "CONSTITUTION",
+    Strength = "STRENGTH",
+    Ranged = "RANGED",
+  }
+
+  interface Assassin {
+    Primary: AttributeNames.Dexterity;
+    Secondary: AttributeNames.Melee;
+    Tertiary: AttributeNames.Charisma;
+  }
+
+  interface Barbarian {
+    Primary: AttributeNames.Strength;
+    Secondary: AttributeNames.Constitution;
+    Tertiary: AttributeNames.Wisdom;
+  }
+
+  interface Bard {
+    Primary: AttributeNames.Charisma;
+    Secondary: AttributeNames.Dexterity;
+    Tertiary: AttributeNames.Intelligence;
+  }
+
+  interface Cleric {
+    Primary: AttributeNames.Wisdom;
+    Secondary: AttributeNames.Constitution;
+    Tertiary: AttributeNames.Strength;
+  }
+
+  interface Druid {
+    Primary: AttributeNames.Wisdom;
+    Secondary: AttributeNames.Constitution;
+    Tertiary: AttributeNames.Charisma;
+  }
+
+  interface Fighter {
+    Primary: AttributeNames.Melee;
+    Secondary: AttributeNames.Strength;
+    Tertiary: AttributeNames.Constitution;
+  }
+
+  interface Monk {
+    Primary: AttributeNames.Dexterity;
+    Secondary: AttributeNames.Wisdom;
+    Tertiary: AttributeNames.Melee;
+  }
+
+  interface Paladin {
+    Primary: AttributeNames.Strength;
+    Secondary: AttributeNames.Charisma;
+    Tertiary: AttributeNames.Melee;
+  }
+
+  interface Ranger {
+    Primary: AttributeNames.Ranged;
+    Secondary: AttributeNames.Dexterity;
+    Tertiary: AttributeNames.Wisdom;
+  }
+
+  interface Shaman {
+    Primary: AttributeNames.Wisdom;
+    Secondary: AttributeNames.Intelligence;
+    Tertiary: AttributeNames.Melee;
+  }
+
+  interface Sorcerer {
+    Primary: AttributeNames.Charisma;
+    Secondary: AttributeNames.Constitution;
+    Tertiary: AttributeNames.Ranged;
+  }
+
+  interface Thief {
+    Primary: AttributeNames.Dexterity;
+    Secondary: AttributeNames.Intelligence;
+    Tertiary: AttributeNames.Charisma;
+  }
+
+  interface Warlock {
+    Primary: AttributeNames.Charisma;
+    Secondary: AttributeNames.Wisdom;
+    Tertiary: AttributeNames.Constitution;
+  }
+
+  interface Wizard {
+    Primary: AttributeNames.Intelligence;
+    Secondary: AttributeNames.Wisdom;
+    Tertiary: AttributeNames.Constitution;
   }
 }
